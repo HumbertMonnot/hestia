084240097bdb3fd332aa05b61628faa86fbff600
BELABBAS FARAH <farah.belabbas@gmail.com>
2022-05-31 17:37:45 +0200

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,19 +19,15 @@ user_2 = User.create!(email: "oceane@gmail.com", password: "secret", username: "
 user_3 = User.create!(email: "humbert@gmail.com", password: "secret", username: "humbert")
 user_4 = User.create!(email: "farah@gmail.com", password: "secret", username: "farah")
 user_5 = User.create!(email: "jojo@gmail.com", password: "secret", username: "jojo")
-
-[user_1, user_2, user_3, user_4, user_5].each do |attributes|
-  User.save!(attributes)
-end
 puts 'finished for users...'
 
 
 puts 'Creating searches...'
-Search.create!(user: user_1 , address:"20 rue gounod 33110 le bouscat", duration: 15, profile: "cycling")
-Search.create!(user: user_2, address:"33 route de verac 33133 galgon", duration: 40, profile: "driving")
-Search.create!(user: user_3, address:"38 rue marsan 33000 bordeaux", duration: 15, profile: "walking")
-Search.create!(user: user_4, address:"5 rue de bouliac 33100 bordeaux", duration: 25, profile: "cycling")
-Search.create!(user: user_5, address:"16 rue des remparts 33000 bordeaux", duration: 25, profile: "walking")
+search_1 = Search.create!(user: user_1 , address:"20 rue gounod 33110 le bouscat", duration: 15, profile: "cycling")
+search_2 = Search.create!(user: user_2, address:"33 route de verac 33133 galgon", duration: 40, profile: "driving")
+search_3 = Search.create!(user: user_3, address:"38 rue marsan 33000 bordeaux", duration: 15, profile: "walking")
+search_4 = Search.create!(user: user_4, address:"5 rue de bouliac 33100 bordeaux", duration: 25, profile: "cycling")
+search_5 =Search.create!(user: user_5, address:"16 rue des remparts 33000 bordeaux", duration: 25, profile: "walking")
 
 puts 'finished for searches...'
 
@@ -50,10 +46,17 @@ indi_11 = IndicatorTitle.create!(name: "commerce de bouche")
 indi_12 = IndicatorTitle.create!(name: "animaux")
 puts 'finished for indicator_titles...'
 
+
 puts 'Creating infrastructures...'
-infra_1 = Infrastructure.create!(indicator_title_id: indi_11, equipment: "boulangerie", domain: "commerces")
-infra_2 = Infrastructure.create!(indicator_title_id: indi_11, equipment: "boucherie,", domain: "commerces")
-infra_3 = Infrastructure.create!(indicator_title_id: indi_11, equipment: "poissonnerie", domain: "commerces")
-infra_4 = Infrastructure.create!(indicator_title_id: indi_10, equipment: "ecole maternelle", domain: "enseignement")
-infra_5 = Infrastructure.create!(indicator_title_id: indi_10, equipment: "College", domain: "enseignement")
+infra_1 = Infrastructure.create!(indicator_title: indi_11, equipment: "boulangerie", domain: "commerces")
+infra_2 = Infrastructure.create!(indicator_title: indi_11, equipment: "boucherie,", domain: "commerces")
+infra_3 = Infrastructure.create!(indicator_title: indi_11, equipment: "poissonnerie", domain: "commerces")
+infra_4 = Infrastructure.create!(indicator_title: indi_10, equipment: "ecole maternelle", domain: "enseignement")
+infra_5 = Infrastructure.create!(indicator_title: indi_10, equipment: "College", domain: "enseignement")
 puts 'finished for infrastructures...'
+
+puts 'Creating Indicators...'
+indicator_1 = Indicator.create!(indicator_title: indi_10, weight: 0.8, search: search_5)
+indicator_2 = Indicator.create!(indicator_title: indi_9, weight: 0.3, search: search_2)
+indicator_3 = Indicator.create!(indicator_title: indi_7, weight: 0.5, search: search_3)
+puts 'finished for Indicators...'
