f1714b456210fe33d9b11ac8e14f83f6a2168393
BELABBAS FARAH <farah.belabbas@gmail.com>
2022-06-01 17:01:44 +0200

    home page V1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,37 @@
-# require "csv"
+require "csv"
 
-# filepath = "bdd_infras_bdx.csv"
+filepath = "bdd_infras_bdx.csv"
 
-# if IndicatorTitle.count == 0
-#   puts 'Creating indicator_title...'
-#   IndicatorTitle.create!(name: "animaux".capitalize)
-#   IndicatorTitle.create!(name: "commerce de bouche".capitalize)
-#   IndicatorTitle.create!(name: "etablissement scolaire".capitalize)
-#   IndicatorTitle.create!(name: "grandes surfaces".capitalize)
-#   IndicatorTitle.create!(name: "installation sportive".capitalize)
-#   IndicatorTitle.create!(name: "médecine courante".capitalize)
-#   IndicatorTitle.create!(name: "médecine spécialisée".capitalize)
-#   IndicatorTitle.create!(name: "petite enfance".capitalize)
-#   IndicatorTitle.create!(name: "restauration".capitalize)
-#   IndicatorTitle.create!(name: "services de proximite".capitalize)
-#   IndicatorTitle.create!(name: "shopping".capitalize)
-#   IndicatorTitle.create!(name: "vie culturelle".capitalize)
-#   puts 'finished for indicator_titles...'
-# end
+if IndicatorTitle.count == 0
+  puts 'Creating indicator_title...'
+  IndicatorTitle.create!(name: "animaux".capitalize)
+  IndicatorTitle.create!(name: "commerce de bouche".capitalize)
+  IndicatorTitle.create!(name: "etablissement scolaire".capitalize)
+  IndicatorTitle.create!(name: "grandes surfaces".capitalize)
+  IndicatorTitle.create!(name: "installation sportive".capitalize)
+  IndicatorTitle.create!(name: "médecine courante".capitalize)
+  IndicatorTitle.create!(name: "médecine spécialisée".capitalize)
+  IndicatorTitle.create!(name: "petite enfance".capitalize)
+  IndicatorTitle.create!(name: "restauration".capitalize)
+  IndicatorTitle.create!(name: "services de proximite".capitalize)
+  IndicatorTitle.create!(name: "shopping".capitalize)
+  IndicatorTitle.create!(name: "vie culturelle".capitalize)
+  puts 'finished for indicator_titles...'
+end
 
-# Infrastructure.destroy_all
+Infrastructure.destroy_all
 
-# CSV.foreach(filepath, headers: :first_row) do |row|
-#   Infrastructure.create!(
-#     longitude: row['lon'],
-#     latitude: row['lat'],
-#     equipment: row['equipment'],
-#     indicator_title_id: row['indicator_title_id']
-#   )
-#   if Infrastructure.count % 8000 == 0
-#     puts Infrastructure.count
-#   end
-# end
+CSV.foreach(filepath, headers: :first_row) do |row|
+  Infrastructure.create!(
+    longitude: row['lon'],
+    latitude: row['lat'],
+    equipment: row['equipment'],
+    indicator_title_id: row['indicator_title_id']
+  )
+  if Infrastructure.count % 8000 == 0
+    puts Infrastructure.count
+  end
+end
 
 puts "cleaning database"
 User.destroy_all
@@ -61,4 +61,3 @@ indicator_1 = Indicator.create!(indicator_title_id: 2, weight: 0.8, search: sear
 indicator_2 = Indicator.create!(indicator_title_id: 3, weight: 0.3, search: search_2)
 indicator_3 = Indicator.create!(indicator_title_id: 7, weight: 0.5, search: search_3)
 puts 'finished for Indicators...'
-
