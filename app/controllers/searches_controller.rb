72b8cbf98d8dc4009a748527377979f67d2670f8
Oceane Feltrin <oceanefeltrin@yahoo.fr>
2022-06-01 16:20:54 +0200

    form back done => front now

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -12,13 +12,16 @@ class SearchesController < ApplicationController
   # Create #create method for searches => no view associated but redirect_to => indicators#edit view
   def create
     @search = Search.new(search_params)
-    @search.user = current_user
+    @search.profile = params[:search][:profile]
+    @search.user = User.last
+
     if @search.save
-      IndicatorTitle.all.each { |indic| Indicator.create(search: @search, indicator_tile: indic) }
+      IndicatorTitle.all.each { |indic| Indicator.create(search: @search, indicator_title: indic) }
       redirect_to search_indicators_path(@search)
     else
       render :new
     end
+
   end
 
   def show
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,9 +1,10 @@
 class Search < ApplicationRecord
+  CATEGORIES = ['walking', 'driving', 'cycling']
   belongs_to :user
   has_many :indicators
   validates :address, presence: true
   validates :duration, presence: true, inclusion: {in: (5..60)}
-  validates :profile, presence: true, inclusion: {in: ['walking', 'driving', 'cycling']}
+  validates :profile, presence: true, inclusion: { in: CATEGORIES }
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
