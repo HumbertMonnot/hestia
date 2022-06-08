1523f9a81bdc267baefdeaf3de838bfceded9790
nicolas perier <nicolas_perier@mailo.com>
2022-06-08 10:49:01 +0200

    echelle des indicators revus

--- app/javascript/controllers/hexas_controller.js
+++ app/javascript/controllers/hexas_controller.js
@@ -14,7 +14,7 @@ export default class extends Controller {
     const my_headers = new Headers({ 'Authorization': `Bearer ${api_token}`, 'Content-Type': 'application/json', 'platformOrigin': "melo" })
     const options = { method: 'GET', withCredentials: true, headers: my_headers}
     fetch(api_base_url, options).then(response => response.json()).then(data => console.log(data))
-    
+
     // On construit la map
     this.map = this.#buildMap()
     this.map.resize()
@@ -28,7 +28,7 @@ export default class extends Controller {
         source: { type: 'geojson', data: data },
         layout: {},
         paint: { "line-color": "#61E294", 'line-width': 5 } });
-    
+
     // On ajoute le marqueur de l'adresse
     this.map.addSource('my-address', {'type': 'geojson', 'data': { 'type': 'FeatureCollection', 'features': [{
       'type': 'Feature',
@@ -37,7 +37,7 @@ export default class extends Controller {
       'coordinates': this.hexalistValue[0]
       }
       }] }});
-    
+
     // Add a symbol layer
     this.map.addLayer({'id': 'my-address', 'type': 'symbol', 'source': 'my-address',
                         'layout': { 'icon-image': 'star-11', 'icon-size': 1.5}});});
@@ -57,8 +57,8 @@ export default class extends Controller {
     var i = 0
     for (const hexa of this.hexas) {
       hexa.properties = await scores[i]
-      i += 1}   
-      
+      i += 1}
+
     // On lisse les scores
     this.#smoothScore(this.hexas, "animaux")
     this.#smoothScore(this.hexas, "commerce_de_bouche")
@@ -83,7 +83,7 @@ export default class extends Controller {
     var endTime = performance.now()
     console.log(endTime - startTime)
   }
-  
+
   // Méthode pour obtenir un isochrone après un appel API Mapbox à partir d'une adresse, une distance et un moyen de transport
   #getIso = async () => {
     const url = `https://api.mapbox.com/isochrone/v1/mapbox/${this.hexalistValue[1]}/${this.hexalistValue[0][0]},${this.hexalistValue[0][1]}?contours_minutes=${this.hexalistValue[2]}&polygons=true&access_token=${this.apiKeyValue}`;
@@ -99,17 +99,20 @@ export default class extends Controller {
     const options = { mask: turf.polygon(polygon) };
     return turf.hexGrid(bbox, cellSide, options).features;
   };
-  
+
   // Méthode pour ramener la meilleure note dans attr à 100
   #smoothScore = (hexas, attr) => {
     let max = 0
+    let min = 0
     hexas.forEach((hexa) => {
       if (hexa.properties[attr] > max) {
         max = hexa.properties[attr]
+      } else if (hexa.properties[attr] < min) {
+        min = hexa.properties[attr]
       }
     })
     const coef = 100 / max
-    hexas.forEach(hexa => hexa.properties[attr] = Math.round(hexa.properties[attr] * coef))
+    hexas.forEach(hexa => hexa.properties[attr] = Math.round((hexa.properties[attr] - min )* coef))
     if (["etablissement_scolaire", "grandes_surfaces"].includes(attr)) {
       hexas.forEach(hexa => hexa.properties[attr] = 100 - hexa.properties[attr])
     }
@@ -119,7 +122,7 @@ export default class extends Controller {
   #weightedAverageScore = (hexas, weights) => {
     hexas.forEach((hexa) => {
       let total = 0
-      let compt = 0 
+      let compt = 0
       for (const property of Object.entries(hexa.properties)) {
         if (compt === 12) break;
         total += hexa.properties[property[0]] * weights[compt]
@@ -165,7 +168,7 @@ export default class extends Controller {
   buildGrid = (hexas_list, the_map, attr) => {
     const hexas_object = {
       features: hexas_list,
-      type: "FeatureCollection" 
+      type: "FeatureCollection"
     }
     // On ajout le calque des hexagones
     the_map.addLayer({
@@ -179,12 +182,12 @@ export default class extends Controller {
       },
     });
 
-    // On ajout le layer symbole (vide) à la map                                                  
+    // On ajout le layer symbole (vide) à la map
     const icon_url = "https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png"
     this.map.loadImage(icon_url, (error, image) => { if (error) throw error;
       this.map.addImage('custom-marker', image);
       this.map.addSource('points', {'type': 'geojson', 'data': { 'type': 'FeatureCollection', 'features': [] }});
-      
+
       // Add a symbol layer
       this.map.addLayer({'id': 'points', 'type': 'symbol', 'source': 'points',
                           'layout': { 'icon-image': 'custom-marker',
@@ -193,7 +196,7 @@ export default class extends Controller {
                                       'text-font': ['Open Sans Semibold'],
                                       'text-offset': [0, 1.25],
                                       'text-anchor': 'top'}});});
-    
+
     // On ajout une action en cas de clique sur un hexagone
     the_map.on('click', async (e) => {
       const selectedFeatures = the_map.queryRenderedFeatures(e.point, {layers: ['maine', 'points']});
@@ -212,7 +215,7 @@ export default class extends Controller {
         for (const selectedFeature of selectedFeatures) {
           if (selectedFeature.layer.id == 'maine') this.#reactToClickHexa(selectedFeature)
           else this.#reactToClickAnnonce(selectedFeature)
-        }            
+        }
       }})
       this.map.moveLayer('my-address')
   }
@@ -231,7 +234,7 @@ export default class extends Controller {
     const api_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImN0eSI6IkpXVCJ9.eyJpYXQiOjE2NTQ1MTE1NzgsImV4cCI6MTY1NTExNjM3OCwiZW1haWwiOiJhcGlfZGVtb19oZXN0eWFAbWVsby5pbyIsImlwIjoiODYuMjEzLjEzLjEyNCIsInVzZXIiOnsiaWQiOiJcL3VzZXJzXC80NTQyNjNmNC01MTgyLTExZWMtYTBhNi03YzEwYzkyMWY1YjMiLCJyb2xlcyI6WyJST0xFX0FQSSIsIlJPTEVfQ1VTVE9NRVIiLCJST0xFX1VTRVIiXSwicGxhdGZvcm0iOnsiaWQiOjIsImlkZW50aWZpZXIiOiJtZWxvIiwibmFtZSI6Ik1lbG8iLCJiYXNlVXJsIjoiaHR0cHM6XC9cL3d3dy5tZWxvLmlvIn19fQ.mctvuXTPKIcDK5Fqa8cJcWHz9cg9vSOg3xLacT_GLoXga50dOALoKa4-jE3BGWzAKgnsvoq55dVLbyJWElTc0KMpR6REi80oMym1WAOsthXZNPuhEfnEgjxH1szFl3uLSdkgZj8e7LEKGtMEkr0LwLUMf4Nh2bMYI0RfXNwx024lkL010Zq8BW-fnC78M3y87VAIB-fcWt4CSIuPW7uxHVdObnSXEseE-Tr_3e3AAEMYQ5A9gSnvbXLtlipXZIswj56ysiJSD-nQPYGHSdM1BuVWSPq_bvq531pCC0WnprkiNuGfCtm0N0vRAT_xkBLAAuikFs7aFRFScszfA7A2pLNF23YtxWPjwblsLevdZofzEx7l7XO3cXWUSSBRDCrpSqNdfih7TsAh4AAkEzYMd94KZwuMPCBeohpGbhdVbFWJ9ABhupu0c8I3TE147Z8XyI1F2U7wHvZ82Cti2yn8DaHAFeP7xu-zWca_0VuezlfQoRZ_AXaNzwxGIiKLrBu8aCNCGUBrCZDU8eOGEsPhS6hgrPxuSUgPaNNqa1r6DO5TLlTff2XYppef4pC9BAkqrBCbqBNifC-nM2iPLsDGgT0KryMGaBloXfTsUWe7jRvFM8w1h3Wwe_5pUj3CxgEdJd7bR5yLsojRrpaQoH8st40S8Tnt0OdiEoSmC55Exho"
     const my_headers = new Headers({'Authorization': `Bearer ${api_token}`, 'Content-Type': 'application/json', 'platformOrigin': "melo"})
     const options = { method: 'GET', withCredentials: true, headers: my_headers}
-      
+
     // On fait la requête
     const response = await fetch(api_base_url, options)
     const data = await response.json()
@@ -249,7 +252,7 @@ export default class extends Controller {
     annonces.forEach((ann) => {annoncesfeatured.push({type: 'Feature',
                                                       geometry: { type: 'Point', coordinates: [ann.coords.lon, ann.coords.lat]},
                                                       properties: ann})})
-    
+
     // On modifie la source
     this.map.getSource('points').setData({ 'type': 'FeatureCollection', 'features': annoncesfeatured });
     }
@@ -306,7 +309,7 @@ export default class extends Controller {
     this.buildGrid(this.hexas, this.map, attr)
     if (this.map.getLayer("points")) {
       this.map.moveLayer('points')
-    }  
+    }
   }
 
   #capitalize = (string) => {
