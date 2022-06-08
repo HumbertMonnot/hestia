b61910994929a5d091066fe5a80f88bda4182a1a
Oceane Feltrin <oceanefeltrin@yahoo.fr>
2022-06-08 12:07:55 +0200

    need to add js to icon for description

---
+++ app/javascript/controllers/link_controller.js
@@ -0,0 +1,9 @@
+
+// link = document.getElementById("link-info")
+// infras = document.getElementById("infras")
+
+// link.addEventListener("click", (event) => {
+//   console.log(event);
+//   console.log(event.currentTarget);
+//   infras.style.display = "block";
+// });
